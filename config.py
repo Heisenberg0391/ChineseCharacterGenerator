de4ba6dacb8d1f19d9ffd0a9ea0c679f8a32cb43
Heisenberg0391 <dumdumslug@hotmail.com>
2019-01-05 11:28:42 +0800

    add larger dict

--- config.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-# 图像保存路径
-IMAGE_PATH = 'outputs/'
-
-# 字体文件路径
-FONT_PATH = 'fonts/'
---
+++ config/cfg.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+# 图像保存路径
+IMAGE_PATH = 'outputs'
+
+# 字体文件路径
+FONT_PATH = 'fonts'
+
+# 字典路径
+dict_path = "config/dict.txt"
